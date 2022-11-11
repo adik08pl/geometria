fa84b98ed6dc2b0eca2d8ce0aae6ecd311aa17ab
Adik08 <adik08.eamil@gmail.com>
2022-11-11 15:44:09 +0100

    brakuje tylko obsugi błedów

--- src/main/java/com/stempien/geometric/HelloController.java
+++ src/main/java/com/stempien/geometric/HelloController.java
@@ -2,6 +2,7 @@ package com.stempien.geometric;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -29,37 +30,78 @@ public class HelloController {
     public Button btnLiczProstokat;
 
     public void LiczTrapez(ActionEvent actionEvent) {
+        try{
         double bokTrapezu1=Double.parseDouble(txtBokTrapezu1.getText().replace(',','.'));
         double bokTrapezu2=Double.parseDouble(txtBokTrapezu2.getText().replace(',','.'));
         double wysokoscTrapezu=Double.parseDouble(txtWysokoscTrapezu.getText().replace(',','.'));
         double poleTrapezu=((bokTrapezu1+bokTrapezu2)*wysokoscTrapezu)/2;
         lblPoleTrapez.setText(String.format("%.2f",poleTrapezu ));
+    }catch (NumberFormatException n){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Błąd");
+        alert.setHeaderText("ERROR");
+        alert.setContentText("Spróbuj jeszcze raz");
+        alert.showAndWait();
+    }
     }
 
     public void LiczKwadrat(ActionEvent actionEvent) {
+        try{
         double bokKwadratu=Double.parseDouble(txtKwadratBok.getText().replace(',','.'));
         double poleKwadratu=bokKwadratu*bokKwadratu;
         lblPoleKwadrat.setText(String.format("%.2f",poleKwadratu ));
+    }catch (NumberFormatException n){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Błąd");
+        alert.setHeaderText("ERROR");
+        alert.setContentText("Spróbuj jeszcze raz");
+        alert.showAndWait();
+    }
     }
 
     public void LiczRownoleglobok(ActionEvent actionEvent) {
+        try{
         double bokRownolegloboku=Double.parseDouble(txtBokRownolegloboku.getText().replace(',','.'));
         double wysokoscRownolegloboku=Double.parseDouble(txtWysokoscRownolegloboku.getText().replace(',','.'));
         double poleRownolegloboku=(bokRownolegloboku*wysokoscRownolegloboku)/2;
         lblPoleRownolegloboku.setText(String.format("%.2f", poleRownolegloboku));
+    }catch (NumberFormatException n){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Błąd");
+        alert.setHeaderText("ERROR");
+        alert.setContentText("Spróbuj jeszcze raz");
+        alert.showAndWait();
+    }
     }
 
     public void LiczRomb(ActionEvent actionEvent) {
+        try{
         double przekatnaRombu1=Double.parseDouble(txtPrzekatnaRombu1.getText().replace(',','.'));
         double przekatnaRombu2=Double.parseDouble(txtPrzekatnaRombu2.getText().replace(',','.'));
         double poleRombu=(przekatnaRombu1*przekatnaRombu2)/2;
         lblPoleRomb.setText(String.format("%.2f", poleRombu));
+    }catch (NumberFormatException n){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Błąd");
+        alert.setHeaderText("ERROR");
+        alert.setContentText("Spróbuj jeszcze raz");
+        alert.showAndWait();
+    }
     }
 
     public void LiczProstokat(ActionEvent actionEvent) {
-        double bokProstokatu1=Double.parseDouble(txtProstokatBok1.getText().replace(',','.'));
-        double bokProstokatu2=Double.parseDouble(txtProstokatBok2.getText().replace(',','.'));
+        try {
+            double bokProstokatu1 = Double.parseDouble(txtProstokatBok1.getText().replace(',', '.'));
+            double bokProstokatu2 = Double.parseDouble(txtProstokatBok2.getText().replace(',', '.'));
+
         double poleProstokatu=bokProstokatu1*bokProstokatu2;
         lblPoleProstokat.setText(String.format("%.2f", poleProstokatu));
+        }catch (NumberFormatException n){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Błąd");
+            alert.setHeaderText("ERROR");
+            alert.setContentText("Spróbuj jeszcze raz");
+            alert.showAndWait();
+        }
     }
 }
